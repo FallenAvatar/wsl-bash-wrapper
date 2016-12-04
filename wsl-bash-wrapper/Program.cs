7ac0869fba54ddf2e791a1c1f91ec4f029b45ae6
Gordon Watts <gordon@gordonandpaula.net>
2016-12-03 23:02:37 -0800

    Added the verbose option

--- wsl-bash-wrapper/Program.cs
+++ wsl-bash-wrapper/Program.cs
@@ -2,18 +2,27 @@
 using System;
 using System.Diagnostics;
 using System.IO;
+using System.Linq;
 using System.Text;
 
 namespace BashWrapper {
     // Run the arguments against bash.
 	class Program {
-		static void Main( string[] args ) {
+		static void Main( string[] bash_args ) {
+
+            // Determine if --verbose was used as the first argument.
+            bool verbose = false;
+            if (bash_args.FirstOrDefault() == "--verbose")
+            {
+                bash_args = bash_args.Skip(1).ToArray();
+                verbose = true;
+            }
 
             var tmp_win_path = Path.GetTempFileName();
             try
             {
                 // Run bash and cache the error code
-                Process p = RunBashAndSaveOutput(args, tmp_win_path);
+                Process p = RunBashAndSaveOutput(bash_args, tmp_win_path, verbose);
                 var ret = p.ExitCode;
 
                 // Unfortunately, under some conditions it seems like the file is locked even after the
@@ -36,11 +45,6 @@ namespace BashWrapper {
 
                 // Report back what we saw happen with the process.
                 Environment.Exit(ret);
-            }
-            catch (Exception e)
-            {
-                Console.WriteLine($"Failed while trying to run and read back log file: {e.Message}");
-                throw;
             }
             finally
             {
@@ -65,7 +69,7 @@ namespace BashWrapper {
             sr.Close();
         }
 
-        private static Process RunBashAndSaveOutput(string[] args, string tmp_win_path)
+        private static Process RunBashAndSaveOutput(string[] args, string tmp_win_path, bool verbose)
         {
             try
             {
@@ -90,8 +94,16 @@ namespace BashWrapper {
 
                 p.StartInfo.FileName = FindBash();
                 p.StartInfo.Arguments = p_args + " &> " + tmp_wsl_path;
+                p.StartInfo.WorkingDirectory = Directory.GetCurrentDirectory();
 
-                p.StartInfo.WorkingDirectory = ConvertPathToWSL(Directory.GetCurrentDirectory());
+                if (verbose)
+                {
+                    Console.WriteLine($"BashWrapper: bash executable {p.StartInfo.FileName}");
+                    Console.WriteLine($"BashWrapper: bash arguments {p.StartInfo.Arguments}");
+                    Console.WriteLine($"BashWrapper: working directory {p.StartInfo.WorkingDirectory}");
+                    Console.WriteLine($"BashWrapper: Temporary output file to capture results: {tmp_win_path}");
+                }
+
                 p.StartInfo.LoadUserProfile = true;
                 p.StartInfo.CreateNoWindow = true;
                 p.StartInfo.UseShellExecute = true;
@@ -99,6 +111,11 @@ namespace BashWrapper {
 
                 p.Start();
                 p.WaitForExit();
+                if (verbose)
+                {
+                    Console.WriteLine($"BashWrapper: bash process exit code: {p.ExitCode}");
+                    Console.WriteLine($"BashWrapper: bash CPU time {p.TotalProcessorTime} and wall clock time {p.ExitTime - p.StartTime}");
+                }
                 return p;
             } catch (Exception e)
             {
